d8a96769c02552471d91e7317504b2d85280862f
Elena Pavlov <linako@gmail.com>
2022-08-01 21:22:26 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -83,8 +83,11 @@ install_dependencies() {
 
   echo "## Installing required Python packages..."
   sudo chown -R $OWNER:$OWNER "$GRAVIS_BASE/env"
+  echo "## Installing required Python packages 1..."
   sudo su $OWNER -c "$GRAVIS_BASE/env/bin/pip install --isolated wheel~=0.37.1"
+  echo "## Installing required Python packages 2..."
   sudo su $OWNER -c "$GRAVIS_BASE/env/bin/pip install --isolated -r \"$GRAVIS_BASE/app/requirements.txt\""
+  echo "## Installing required Python packages 3..."
 }
 
 systemd_install () {
