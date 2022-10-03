94d2de07ccc388eafd638cece289570442c66c51
linako111 <linako@gmail.com>
2022-10-03 04:05:00 +0000

    Added .lock file, cleaning code.

---
+++ common/helper.py
@@ -0,0 +1,28 @@
+"""
+helper.py
+=========
+Various internal helper functions for gravis.
+"""
+# Standard python includes
+from pathlib import Path
+
+
+class FileLock:
+    """Helper class that implements a file lock. The lock file will be removed also from the destructor so that
+    no spurious lock files remain if exceptions are raised."""
+
+    def __init__(self, path_for_lockfile: Path):
+        self.lockCreated = True
+        self.lockfile = path_for_lockfile
+        self.lockfile.touch(exist_ok=False)
+
+    # Destructor to ensure that the lock file gets deleted
+    # if the calling function is left somewhere as result
+    # of an unhandled exception
+    def __del__(self) -> None:
+        self.free()
+
+    def free(self) -> None:
+        if self.lockCreated:
+            self.lockfile.unlink()
+            self.lockCreated = False
--- portal/jobs/watch_incoming.py
+++ portal/jobs/watch_incoming.py
@@ -8,12 +8,14 @@ import pydicom
 from django.conf import settings
 
 from portal.models import Case, DICOMInstance, DICOMSet
-from portal.common.generate_folder_name import generate_folder_name
+from common.generate_folder_name import generate_folder_name
+import common.helper as helper
+
 
 logger = logging.getLogger(__name__)
 
 
-def process_folder(incoming_case):
+def process_folder(incoming_case: Path):
     data_folder = Path(settings.DATA_FOLDER)
     cases = data_folder / "cases"
 
@@ -25,17 +27,44 @@ def process_folder(incoming_case):
     processed_dest_folder = new_folder / "processed"
     findings_dest_folder = new_folder / "findings"
 
+    lock_file = Path(incoming_case) / ".lock"
+    if lock_file.exists():
+        # Series is locked, so another instance might be working on it
+        return
+
+    # Create lock file in the incoming folder and prevent other instances from working on this series
+    try:
+        lock = helper.FileLock(lock_file)
+    except FileExistsError:
+        # Series likely already processed by other instance of router
+        return
+    except:
+        # Can't create lock file, so something must be seriously wrong
+        logger.error(
+            f"Unable to create lock file {lock_file} in {incoming_case}"
+        )  # handle_error
+        return
+
     try:
         input_dest_folder.mkdir(parents=True, exist_ok=False)
+    except Exception as e:
+        print(f"Exception {e}. Cannot create {input_dest_folder}")
+        return False
+
+    try:
         processed_dest_folder.mkdir(parents=True, exist_ok=False)
+    except Exception as e:
+        print(f"Exception {e}. Cannot create {processed_dest_folder}")
+        return False
+
+    try:
         findings_dest_folder.mkdir(parents=True, exist_ok=False)
     except Exception as e:
-        print(f"Exception {e}. Cannot generate {input_dest_folder}")
+        print(f"Exception {e}. Cannot create {findings_dest_folder}")
         return False
 
     # Move files
     try:
-        # TODO: test if there are subfolders
         files_to_copy = incoming_case.glob("**/*")
         for file_path in files_to_copy:
             dst_path = os.path.join(input_dest_folder, os.path.basename(file_path))
@@ -47,6 +76,13 @@ def process_folder(incoming_case):
             f"Exception {e} during copying files from {incoming_case} to {input_dest_folder}"
         )
         return False
+
+    # Delete empty folder from incoming
+    try:
+        os.rmdir(incoming_case)
+    except Exception as e:
+        print(f"Exception {e} trying to delete an empty {incoming_case}")
+        # return False
 
     # Read study.jon from the incoming folder
     json_name = "study.json"
@@ -72,7 +108,7 @@ def process_folder(incoming_case):
             twix_id=payload["twix_id"],
             case_location=str(new_folder),
             incoming_payload=payload,
-        )  # Case(data_location="/data/cases/<foo>")
+        )  # Case(data_location="./data/cases/[UID]")
         new_case.save()
     except Exception as e:
         print(f"Exception during Case model creation: {e}")
@@ -115,6 +151,14 @@ def process_folder(incoming_case):
             print(f"Cannot process incoming instance {str(dcm)}")
             continue
 
+    try:
+        lock.free()
+    except Exception:
+        logger.error(
+            f"Unable to remove lock file {lock_file}" in {incoming_case}
+        )  # handle_error
+        return
+
     print(f"Done Processing {incoming_case}")
     return True
 
@@ -133,11 +177,13 @@ def scan_incoming_folder():
 
     for incoming_case in list(folder_paths):
         process_folder(incoming_case)
+
         # TODO: if return is false move it to the error folder
         # else set status of the case to Case.CaseStatus.QUEUED
 
 
 def watch():
+    print("AAA watch()")
     while True:
         sleep(1)
         try:
