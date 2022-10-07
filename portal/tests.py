c823515134d0c00d6e8a9a10458f5a19830ec7a3
Roy Wiggins <roywig@gmail.com>
2022-10-07 19:04:55 +0000

    add pytest

--- portal/tests.py
+++ portal/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+import pytest
+from .models import *
+from django.conf import settings
+from pathlib import Path 
+from pyfakefs.fake_filesystem import FakeFilesystem
 
-# Create your tests here.
+def test_example(fs: FakeFilesystem):
+    fs.create_dir(Path(settings.INCOMING_FOLDER) / "test" )
+    assert (Path(settings.INCOMING_FOLDER) / "test").exists()
