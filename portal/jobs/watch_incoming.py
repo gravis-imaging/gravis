e1ec92cc12e5bd12812d661ed24a0b3d3892b3e0
Roy Wiggins <roywig@gmail.com>
2022-11-08 18:55:47 +0000

    fixup

--- portal/jobs/watch_incoming.py
+++ portal/jobs/watch_incoming.py
@@ -339,17 +339,17 @@ def trigger_queued_cases():
 
 
 def watch():
-    logging.info("Incoming watcher booted.")
+    logger.info("Incoming watcher booted.")
     while True:
         sleep(settings.INCOMING_SCAN_INTERVAL)
         try:
             scan_incoming_folder()
         except:
-            logger.error("Failure in incoming")
+            logger.exception("Failure in incoming")
 
         try:
             trigger_queued_cases()
         except:
-            logger.error("Error in queuing.")
+            logger.exception("Error in queuing.")
 
     # get_queue("default").enqueue_in(timedelta(seconds=2), watch)
