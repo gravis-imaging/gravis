5230d220b724f964734125369054bf440df25b5e
Roy Wiggins <roywig@gmail.com>
2023-05-04 21:47:29 +0000

    use mercureimaging/gravis-metsmaps:main

--- portal/jobs/pipelines/onco.py
+++ portal/jobs/pipelines/onco.py
@@ -4,6 +4,6 @@ from portal.jobs.mark_case_ready import MarkCaseReadyJob
 
 def run(case):
     incoming_dicom_set = case.dicom_sets.get(origin="Incoming")    
-    mets_job, rq_mets_job = DockerJob.enqueue_work(case, incoming_dicom_set, docker_image="jmsocallaghan/gravis-metsmaps")
+    mets_job, rq_mets_job = DockerJob.enqueue_work(case, incoming_dicom_set, docker_image="mercureimaging/gravis-metsmaps:main")
     _, rq_preview_job = GeneratePreviewsJob.enqueue_work(case, incoming_dicom_set)
     MarkCaseReadyJob.enqueue_work(case, depends_on=[rq_mets_job, rq_preview_job])
