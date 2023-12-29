5b1cc4a7859c063a98a114e6f4aa0215415b7be9
Roy Wiggins <roywig@gmail.com>
2023-12-29 03:18:29 +0000

    work on install script

--- install.sh
+++ install.sh
@@ -64,7 +64,7 @@ install_app_files() {
 install_packages() {
   echo "## Installing Linux packages..."
   sudo apt-get --assume-yes update
-  sudo apt-get --assume-yes install -y software-properties-common build-essential sqlite3 python3.10-dev python3.10-venv
+  sudo apt-get --assume-yes install -y software-properties-common build-essential sqlite3 python3.10-dev python3.10-venv postgresql nginx redis
   # sudo apt --assume-yes upgrade
 }
 
@@ -81,29 +81,59 @@ install_dependencies() {
   sudo su $OWNER -c "$GRAVIS_BASE/venv/bin/pip install --isolated -r \"$GRAVIS_BASE/app/requirements.txt\""
 }
 
+install_services() {
+   echo "## Installing services..."
+  sudo cp "$GRAVIS_SRC"/install/*.{service,socket} /etc/systemd/system
+  sudo systemctl enable gravis-gunicorn.service gravis-gunicorn.socket gravis-watcher.service gravis-worker.service gravis-worker-cheap.service gravis.service
+  sudo systemctl start gravis
+}
+
+install_postgres() {
+  echo "## Setting up postgres..."
+  sudo -u postgres -s <<- EOM
+    cd ~
+    createuser gravis || true
+    createdb gravis -O gravis || true
+EOM
+  echo "## Running initial migrations..."
+  sudo -u gravis -s <<- EOK
+  cd $GRAVIS_BASE/app
+  ../venv/bin/python manage.py migrate
+EOK
+}
+
+install_nginx() {
+  echo "## Nginx"
+  sudo cp $GRAVIS_SRC/install/gravis_nginx.conf /etc/nginx/sites-available
+  sudo ln -s /etc/nginx/sites-available/gravis_nginx.conf /etc/nginx/sites-enabled
+  sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=US/ST=NY/L=NYC/O=GRAVIS/CN=gravis.local" -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
+  sudo service nginx reload
+}
+
 systemd_install () {
   echo "## Performing systemd-type gravis installation..."
-#   create_user
+  create_user
   create_folders
   install_packages
   install_app_files
   install_dependencies
+  install_postgres
+  install_nginx
   sudo chown -R $OWNER:$OWNER "$GRAVIS_BASE"
 }
 
 systemd_install
 
-sudo chown -R vagrant .
-
-python_l="/usr/bin/python"
-if [ -L ${python_l} ] ; then
-   if [ ! -e ${python_l} ] ; then
-      sudo ln -s /usr/bin/python3 /usr/bin/python
-   fi
-elif [ -e ${python_l} ] ; then
-   echo "Not a link"
-else
-   sudo ln -s /usr/bin/python3 /usr/bin/python
-fi
+# sudo chown -R vagrant .
+# python_l="/usr/bin/python"
+# if [ -L ${python_l} ] ; then
+#    if [ ! -e ${python_l} ] ; then
+#       sudo ln -s /usr/bin/python3 /usr/bin/python
+#    fi
+# elif [ -e ${python_l} ] ; then
+#    echo "Not a link"
+# else
+#    sudo ln -s /usr/bin/python3 /usr/bin/python
+# fi
 
 echo "Installation complete"
